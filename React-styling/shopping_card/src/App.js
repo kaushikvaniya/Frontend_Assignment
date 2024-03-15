0a96b78ddeb4b7fb8a79fb089dd9a54bc803e1fb
kaushikvaniya <kaushikvaniya907@gmail.com>
2024-03-15 11:16:40 +0530

    React_styling

---
+++ React-styling/shopping_card/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Homesection from "./Components/Homesection/Homesection";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Homesection />
+    </>
+  );
+}
+
+export default App;
